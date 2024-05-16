2a742f4f6a6891d17bc6a2aa556161806be95529
vy-truong <trminhvy@gmail.com>
2024-05-16 15:03:59 -0600

    commit test

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import StudentInfo from "./student-info"; 
+
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+
+       <StudentInfo/> 
+       {/* <Link href="../">Back</Link> */}
+        
       </main>
     );
+    
   } 
+
+
+  
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,11 +1,17 @@
-function StudentInfo() {
+import Link from "next/link";
+
+
+export default function StudentInfo() {
 
     const name = "Vy Truong";
-    const myGitHub = "https://github.com/vy-truong";
+    const gitHub = "https://github.com/vy-truong/cprg306-assignments";
     return (
         <main>
             <h1>{name}</h1>
-            <p>Link to my GitHub</p>
+            <p>Vy pronounces as the letter V</p>
+            {/* <Link className="underline text-cyan-600  hover:text-cyan-300" href={gitHub}>My GitHub repository</Link> */}
+            <Link  href={gitHub} className="underline text-cyan-600  hover:text-cyan-300">My GitHub repository</Link>
         </main>
+       
     );
 }
