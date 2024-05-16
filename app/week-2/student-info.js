226714d64612f752807c2a0cb79fffede973d83c
vy-truong <trminhvy@gmail.com>
2024-05-16 12:27:43 -0600

    maow

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+function StudentInfo() {
+
+    const name = "Vy Truong";
+    const myGitHub = "https://github.com/vy-truong";
+    return (
+        <main>
+            <h1>{name}</h1>
+            <p>Link to my GitHub</p>
+        </main>
+    );
+}
