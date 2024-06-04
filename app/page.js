4f4044106679916a23569340cc90fe71907af95d
vy-truong <trminhvy@gmail.com>
2024-06-03 22:09:27 -0600

    commit a3

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
-
+ 
 export default function Home() {
   return (
       <main>
-        <h1>CPRG 306: Web Development 2 - Assignments</h1>
-        <Link className="underline text-cyan-600  hover:text-cyan-300" href="./week-2" >Week-2</Link>
+        <h1>CPRG 306:  Web Development 2 - Assignments</h1>
+        <div>
+          <Link className="underline text-cyan-600 hover:text-cyan-300" href="./week-2">Week 2 Assignment</Link>
+        </div>
+        <div>
+          <Link className="underline text-cyan-600 hover:text-cyan-300" href="./week-3">Week 3 Assignment</Link>
+        </div>
       </main>
   );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import ItemList from "./item-list";
+ 
+ 
+export default function Page(){
+    return(
+        <main>
+            {/*  */}
+            <div className="bg-slate-800 p-4" >
+            <h1 className="text-3xl text-white font-bold mx-4 p-4">Shopping List</h1>  
+            <ItemList/>
+            </div>
+        </main>
+    );
+}
