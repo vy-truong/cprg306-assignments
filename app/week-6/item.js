8329647f853da493cbecbe3eed74aa0667402982
Vy Truong <trminhvy@gmail.com>
2024-08-07 16:46:33 -0600

    update1
    copying w5

---
+++ app/week-6/item.js
@@ -0,0 +1,23 @@
+
+"use client"
+import React from "react";
+export default function Item({ item }) {
+    //des
+    const { name, quantity, category } = item;
+ 
+    const itemStyle = "bg-blue-400 text-white p-4 border-b border-gray-700 m-5  my-2 max-w-xl";
+    const nameStyle = "text-lg font-bold";
+    const detailStyle = "text-gray-900";
+ 
+    return(
+        <main>
+            <ul className={itemStyle}>
+                <li className={nameStyle}>{name}</li>
+                <li  className={detailStyle}>Buy {quantity} in {category}</li>
+            </ul>
+        </main>
+   
+    );
+
+
+}
